b505cd8ec8a0282c64cbf164cfb208e485aa58b5
ElvisLin125 <elvis.125.ma@gmail.com>
2021-02-02 00:12:58 -0800

    Added DatePicker from AntDesign

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import './styles/main.scss';
 //import Task from './components/task.js';
 import Timer from './components/timer.js';
-
+import {DatePicker} from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="background">
         <div className="timerContainer" /*contentEditable*/>
           <Timer/>
+          <DatePicker></DatePicker>
         </div>
 
 
